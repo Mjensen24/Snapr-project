a7928c6c311e8ea16ace38a4328a5e71daa289f3
Michael Jensen <mcjensen24@gmail.com>
2021-02-08 12:23:42 -0500

    created heroku production database url

--- backend/config/database.js
+++ backend/config/database.js
@@ -16,7 +16,7 @@ module.exports = {
     seederStorage: "sequelize",
   },
   production: {
-    use_env_variable: "DATABASE_URL",
+    use_env_variable: "postgres://odbdrxuvlphqff:47a6936a94901706e78b1627782282b3cbb8298dc62dcf31110d6c2eb4ea9766@ec2-54-164-241-193.compute-1.amazonaws.com:5432/d1o9f34qs27sis",
     dialect: "postgres",
     seederStorage: "sequelize",
   },
